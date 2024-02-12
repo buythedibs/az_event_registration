1e7e20a839237b4e553b79df422156c8cf6b52af
Steven Chang <stevenchang5000@protonmail.com>
2024-02-12 16:24:53 +1000

    fn register without the emit (#2)

--- lib.rs
+++ lib.rs
@@ -1,8 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
+mod errors;
+
 #[ink::contract]
 mod az_event_registration {
-    use ink::storage::Mapping;
+    use crate::errors::AzEventRegistrationError;
+    use ink::{reflect::ContractEventBase, storage::Mapping};
+
+    // === TYPES ===
+    type Event = <AzEventRegistration as ContractEventBase>::Type;
+    type Result<T> = core::result::Result<T, AzEventRegistrationError>;
 
     // === STRUCTS ===
     #[derive(Debug, Clone, scale::Encode, scale::Decode)]
@@ -17,9 +24,8 @@ mod az_event_registration {
         derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout)
     )]
     pub struct Registration {
-        id: u32,
         address: AccountId,
-        referrer: AccountId,
+        referrer: Option<AccountId>,
     }
 
     // === CONTRACT ===
@@ -42,6 +48,41 @@ mod az_event_registration {
         pub fn config(&self) -> Config {
             Config { admin: self.admin }
         }
+
+        #[ink(message)]
+        pub fn show(&self, address: AccountId) -> Result<Registration> {
+            self.registrations
+                .get(address)
+                .ok_or(AzEventRegistrationError::NotFound(
+                    "Registration".to_string(),
+                ))
+        }
+
+        // === HANDLES ===
+        #[ink(message)]
+        pub fn register(&mut self, referrer: Option<AccountId>) -> Result<Registration> {
+            let caller: AccountId = Self::env().caller();
+            if let Some(referrer_unwrapped) = referrer {
+                if referrer_unwrapped == caller {
+                    return Err(AzEventRegistrationError::UnprocessableEntity(
+                        "Registrant and referrer must be different".to_string(),
+                    ));
+                }
+            }
+            if self.registrations.get(caller).is_some() {
+                return Err(AzEventRegistrationError::UnprocessableEntity(
+                    "Registration already exists".to_string(),
+                ));
+            }
+
+            let registration = Registration {
+                address: caller,
+                referrer,
+            };
+            self.registrations.insert(caller, &registration);
+
+            Ok(registration)
+        }
     }
 
     #[cfg(test)]
@@ -56,18 +97,70 @@ mod az_event_registration {
         fn init() -> (DefaultAccounts<DefaultEnvironment>, AzEventRegistration) {
             let accounts = default_accounts();
             set_caller::<DefaultEnvironment>(accounts.bob);
-            let az_smart_contract_hub = AzEventRegistration::new();
-            (accounts, az_smart_contract_hub)
+            let az_event_registration = AzEventRegistration::new();
+            (accounts, az_event_registration)
         }
 
         // === TESTS ===
         // === TEST QUERIES ===
         #[ink::test]
         fn test_config() {
-            let (accounts, az_smart_contract_hub) = init();
-            let config = az_smart_contract_hub.config();
+            let (accounts, az_event_registration) = init();
+            let config = az_event_registration.config();
             // * it returns the config
             assert_eq!(config.admin, accounts.bob);
+        }
+
+        // === TEST HANDLES ===
+        #[ink::test]
+        fn test_register() {
+            let (accounts, mut az_event_registration) = init();
+            // when registration does not exist
+            // = when referrer is present
+            // == when referrer is different to caller
+            // === * it create a new registration
+            let mut referrer: Option<AccountId> = Some(accounts.alice);
+            let mut result = az_event_registration.register(referrer);
+            let mut result_unwrapped = result.unwrap();
+            assert_eq!(
+                result_unwrapped,
+                Registration {
+                    address: accounts.bob,
+                    referrer
+                }
+            );
+            // == when referrer is same as caller
+            referrer = Some(accounts.bob);
+            // === * it raises an error
+            result = az_event_registration.register(referrer);
+            assert_eq!(
+                result,
+                Err(AzEventRegistrationError::UnprocessableEntity(
+                    "Registrant and referrer must be different".to_string()
+                ))
+            );
+            // = when referrer is blank
+            referrer = None;
+            // = * it create a new registration
+            set_caller::<DefaultEnvironment>(accounts.charlie);
+            result = az_event_registration.register(referrer);
+            result_unwrapped = result.unwrap();
+            assert_eq!(
+                result_unwrapped,
+                Registration {
+                    address: accounts.charlie,
+                    referrer
+                }
+            );
+            // when registration exists
+            // * it raises an error
+            result = az_event_registration.register(referrer);
+            assert_eq!(
+                result,
+                Err(AzEventRegistrationError::UnprocessableEntity(
+                    "Registration already exists".to_string()
+                ))
+            );
         }
     }
 }
